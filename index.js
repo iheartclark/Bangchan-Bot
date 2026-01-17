a4eb61502b3405479e9c61293888099611647619
iheartclark <sschna4pp@gmail.com>
2026-01-16 21:45:18 -0300

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+📦 mi-bot
+ ┣ package.json        ← Dependencias y configuración de Node.js
+ ┣ index.js            ← Código principal de tu bot
+ ┣ config.json         ← Token y configuraciones privadas
+ ┣ /data               ← Carpeta para almacenar usuarios y cartas
+ ┃   ┗ users.json
+ ┃   ┗ cards.json
